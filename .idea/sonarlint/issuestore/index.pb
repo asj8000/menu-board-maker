a440e4a7ce4eaa000ed935e15dae4a06a7950e8d
asj <asj8000@gmail.com>
2023-12-17 14:05:19 +0900

    feat: set default layout

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,23 @@
 
 9
-	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+src/app/layout.js,b\7\b7c052a7d21b1daeb1c65f186a3f5898d9c80896
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+B
+src/pages/index.js,c\1\c16ef34da304c300e7b6c91b3a47fea77c278c7a
+E
+src/app/components.js,6\9\69e90c2aff5a69c505c16a52eae94b626ac8d0ff
+B
+tailwind.config.js,0\9\0918cbb76f2c44f223706cec9d31a32cd1177ce4
+C
+src/app/globals.css,d\a\da1d6f21d01d0f36b5501dffcf7fd160ffcb290d
+?
+src/app/page.js,8\1\81bad8b6440a0d75bcac083dfaaf1694bd9ce8b8
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+A
+package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
---
+++ src/app/components.js
@@ -0,0 +1,214 @@
+export default function HomeComponent() {
+
+  return (
+    <div className="w-screen h-screen overflow-hidden flex">
+      <div className="w-4/6">
+        <div className="p-8 h-1/6">
+          <h1 className="text-3xl font-bold">가나다라 네일샵</h1>
+          <div className="flex space-x-4 mt-4">
+            <div className="bg-[#e6f7ff]">손가락</div>
+            <div className="bg-[#e6f7ff]">기본 케어</div>
+            <div className="bg-[#e6f7ff]">제거</div>
+            <div className="bg-[#e6f7ff]">컬러링</div>
+            <div className="bg-[#e6f7ff]">네일케어</div>
+          </div>
+        </div>
+        <div className="h-5/6 overflow-y-scroll">
+          <div className="grid grid-cols-4 gap-4 p-4 bg-gray-200 ">
+            <div className="bg-[#ccf2ff] rounded-xl p-4 ">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어 + 컬러링 + 영양제</p>
+                <p>가나다라네일샵</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 컬러링</p>
+                <p>이제 색을 입혀보아요</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어</p>
+                <p>가나다라네일샵</p>
+                <p>+ 영양제</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어</p>
+                <p>가나다라네일샵</p>
+                <p>+ 영양제</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어</p>
+                <p>가나다라네일샵</p>
+                <p>+ 영양제</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어</p>
+                <p>가나다라네일샵</p>
+                <p>+ 영양제</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ccf2ff] rounded-xl p-4">
+              <div>
+                <p>손 기본 케어</p>
+                <p>케어</p>
+                <p>가나다라네일샵</p>
+                <p>+ 영양제</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ffd6cc] rounded-xl p-4">
+              <div>
+                <p>제거</p>
+                <p className="mt-4">0원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손 연장</p>
+                <p>실물과 맞는 매치지</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손님맞춤</p>
+                <p>bts 노래추천</p>
+                <p>실물과 맞는 매치지</p>
+                <p>컬러링</p>
+                <p>제거</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ffd6cc] rounded-xl p-4">
+              <div>
+                <p>제거</p>
+                <p className="mt-4">0원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손 연장</p>
+                <p>실물과 맞는 매치지</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손님맞춤</p>
+                <p>bts 노래추천</p>
+                <p>실물과 맞는 매치지</p>
+                <p>컬러링</p>
+                <p>제거</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#ffd6cc] rounded-xl p-4">
+              <div>
+                <p>제거</p>
+                <p className="mt-4">0원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손 연장</p>
+                <p>실물과 맞는 매치지</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손님맞춤</p>
+                <p>bts 노래추천</p>
+                <p>실물과 맞는 매치지</p>
+                <p>컬러링</p>
+                <p>제거</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+            <div className="bg-[#fffacc] rounded-xl p-4">
+              <div>
+                <p>손님맞춤</p>
+                <p>bts 노래추천</p>
+                <p>실물과 맞는 매치지</p>
+                <p>컬러링</p>
+                <p>제거</p>
+                <p className="mt-4">40,000원</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-2/6 bg-[#f5f5f5] p-8 flex flex-col justify-between">
+        <div className="mb-4">
+          <h2 className="text-xl font-bold">주문</h2>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <p>손 기본 케어</p>
+              <p>40,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>제거</p>
+              <p>6,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>노래 추천 (500원)</p>
+              <p>4,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>파츠 제거 (1000원)</p>
+              <p>2,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>아트</p>
+              <p>8,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>로코코 스타일 2000원</p>
+              <p>8,000원</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>오렌지 가득</p>
+              <p>0원</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center justify-between mt-4">
+            <div variant="outline">판매</div>
+            <div className="flex items-center">
+              <div variant="outline">-</div>
+              <span className="mx-2">1</span>
+              <div variant="outline">+</div>
+            </div>
+          </div>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <p>주문금액 (3개) :</p>
+              <p>54,000원</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
